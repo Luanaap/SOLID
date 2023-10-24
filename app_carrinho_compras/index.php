dae4f5a3794041f4f0e82cdd3b5843c8a51bffd4
Luanaap <57630311+Luanaap@users.noreply.github.com>
2023-10-24 20:29:17 -0300

    first commit

---
+++ app_carrinho_compras/index.php
@@ -0,0 +1,30 @@
+<?php
+
+require __DIR__."/vendor/autoload.php";
+
+use App\CarrinhoCompra;
+
+$carrinho1 = new CarrinhoCompra();
+
+print_r($carrinho1->exibirItens());
+echo 'Valor total:  ' .$carrinho1->exibirValorTotal();
+
+$carrinho1->adicionarItem('Bicicleta', 750,10);
+$carrinho1->adicionarItem('Geladeira', 1950,15);
+$carrinho1->adicionarItem('Tapete', 350,20);
+
+echo "<br />";
+print_r($carrinho1->exibirItens());
+echo 'Valor total recalculado: ' .$carrinho1->exibirValorTotal();
+
+echo "<br />";
+echo 'status: ' .$carrinho1->exibirStatus();
+
+echo "<br />";
+if($carrinho1->confirmarPedido()){
+    echo 'Pedido realizado com sucesso!';
+} else {
+    echo 'Erro na confirmação de pedido. Carrinho está vazio';
+};
+echo "<br />";
+echo 'status: ' .$carrinho1->exibirStatus();
